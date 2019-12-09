ca16c96f88ead5f9e013b0eeab0e60563eb4d6b5
Jude M Battista <jbattista20@my.whitworth.edu>
2019-12-09 01:31:16 -0800

    Tried more things. Nothing worked. Hope is dead.

--- src/pokemon/Abilities.tsx
+++ src/pokemon/Abilities.tsx
@@ -25,6 +25,7 @@ class Abilities extends Component<Props, State> {
   }
 
   // Does not execute prior to handle
+  /*
   getSnapshotBeforeUpdate(prevProps) {
     if (this.props.query === prevProps.query) {
       return;
@@ -33,8 +34,47 @@ class Abilities extends Component<Props, State> {
       this.setState({abilitiesRetrieved: false});
     }
   }
+  */
 
   componentDidUpdate() {
+
+  }
+
+  handleResponse = (error: any, response: any, body: any) => {
+    // grab the description and name
+    let jsonBody = JSON.parse(body);
+    let description = jsonBody.effect_entries[0].short_effect;
+    let name = jsonBody.name;
+
+    // add to the key value pairs list
+
+    // Since abilities can have undefined entries in the API, only count the valid ones
+    // We would LIKE to remove them but React won't let us.
+    let abilityCount = 0;
+    for (const ability of this.props.abilities) {
+      if(ability) {
+        abilityCount++;
+      }
+    }
+
+    if (abilitiesList.length < abilityCount) {
+      abilitiesList.push([name, description]);
+    }
+    console.log(`added ${name}:${description} to ${abilitiesList}`);
+    this.setState({ abilitiesRetrieved: true });
+  }
+
+  render() {
+    console.log(`Props query: ${this.props.query}, previous query: ${prevQuery}`);
+    if (this.props.query !== prevQuery) {
+      abilitiesList = [];
+      prevQuery = this.props.query;
+      this.setState({abilitiesRetrieved: false});  
+      console.log(`Inside if, props query: ${this.props.query}, previous query: ${prevQuery}, abilities list: ${abilitiesList}`);
+    } else {
+      console.log(`Inside else, props query: ${this.props.query}, previous query: ${prevQuery}, abilities list: ${abilitiesList}`);
+    }
+    
     if (!this.state.abilitiesRetrieved) {
       for (const ability of this.props.abilities) {
         if (typeof ability === "undefined") { // there may be gaps in the props.abilities array. just ignore these
@@ -59,39 +99,7 @@ class Abilities extends Component<Props, State> {
         request(options, this.handleResponse);
       }
     }
-  }
 
-  handleResponse = (error: any, response: any, body: any) => {
- 
-    // grab the description and name
-    let jsonBody = JSON.parse(body);
-    let description = jsonBody.effect_entries[0].short_effect;
-    let name = jsonBody.name;
-
-    // add to the key value pairs list
-
-    // Since abilities can have undefined entries in the API, only count the valid ones
-    // We would LIKE to remove them but React won't let us.
-    let abilityCount = 0;
-    for (const ability of this.props.abilities) {
-      if(ability) {
-        abilityCount++;
-      }
-    }
-
-    if (abilitiesList.length < abilityCount) {
-      abilitiesList.push([name, description]);
-    }
-
-    this.setState({ abilitiesRetrieved: true });
-  }
-
-  render() {
-    if (this.props.query !== prevQuery) {
-      abilitiesList = [];
-    }
-
-    prevQuery = this.props.query;
 
     let abilityComponent: any;
     if (this.state.abilitiesRetrieved) {
